e0f1a6a3bd3d21bcdf9425410644a879532020a3
Vals098 <valeria.farinosi@gmail.com>
2026-05-08 14:31:49 +0200

    add: weather details in details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,15 +1,43 @@
 import NavBar from "../components/NavBar"
 import { useParams } from "react-router-dom"
-
+import { useEffect, useState } from "react"
 
 const Details = function () {
-const {cityName} = useParams()
+  const { cityName } = useParams()
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=e7282535f1801102596260041d76bf77&units=metric`,
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error("Error fetching weather")
+        }
+      })
+      .then((data) => {
+        setWeather(data)
+      })
+      .catch(
+        (error) => {
+          console.log(error)
+        },
+        [cityName],
+      )
+  })
 
   return (
     <>
       <NavBar />
-      <h1>{cityName}</h1>
-
+      {weather && (
+        <div>
+          <h1>{weather.name}</h1>
+          <h2>{weather.main.temp}°C</h2>
+          <p>{weather.weather[0].description}</p>
+        </div>
+      )}
     </>
   )
 }
